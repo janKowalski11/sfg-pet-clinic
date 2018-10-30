95a5f2022db6bca3d34f12ddc3e6f22df23d0b47
BeGieU <marcinoo890@gmail.com>
2018-10-30 17:49:44 +0100

    26 - after episode 127

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.services;
+/*
+Author: BeGieU
+Date: 30.10.2018
+*/
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends  CrudService<PetType,Long>
+{
+
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,45 @@
+package guru.springframework.sfgpetclinic.services.map;
+/*
+Author: BeGieU
+Date: 30.10.2018
+*/
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService
+{
+    @Override
+    public Set<PetType> findAll()
+    {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id)
+    {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object)
+    {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object)
+    {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id)
+    {
+        super.deleteById(id);
+    }
+}
