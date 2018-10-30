8e6291b2834fb3a8b96230c450bd7eed50fcb1c5
BeGieU <marcinoo890@gmail.com>
2018-10-30 17:35:54 +0100

    25 - after episode 126

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -4,6 +4,21 @@ Author: BeGieU
 Date: 14.10.2018
 */
 
+import java.util.Set;
+
 public class Vet extends Person
 {
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities()
+    {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities)
+    {
+        this.specialities = specialities;
+    }
+
+
 }
