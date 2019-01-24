dbaacd77a69752f25e6741cd9eccb7a7e05a0048
BeGieU <marcinoo890@gmail.com>
2019-01-24 13:54:04 +0100

    added some protection to Post methods

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -8,7 +8,9 @@ import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,6 +32,15 @@ public class OwnerController
     public OwnerController(OwnerService ownerService)
     {
         this.ownerService = ownerService;
+    }
+
+    /*Zabezpieczenie ktore sprawia ze nie ma mozliwosci wykonac metody @Post
+    * na polu Id. Czyli uzytkownik strony nie moze ustawic w zaden
+    * sposob id obiektu wysylanego do DB. Tylko DB moze zmieniac id*/
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder)
+    {
+        dataBinder.setDisallowedFields("id");
     }
 
     @RequestMapping({"", "/", "/index,", "/index.html"})
