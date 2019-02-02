37126c6c781f38e329e1b24f406e835967964b94
BeGieU <marcinoo890@gmail.com>
2019-02-02 15:29:05 +0100

    added json endpoint

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -4,11 +4,16 @@ Author: BeGieU
 Date: 17.10.2018
 */
 
+import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController
@@ -29,6 +34,13 @@ public class VetController
 
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson()
+    {
+
+        return vetService.findAll();
     }
 
 }
